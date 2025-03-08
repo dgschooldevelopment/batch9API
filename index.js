18ecbc9e3a87f273198602ca7ceea23b5be0795a
dgschooldevelopment <dreamsguider@gmail.com>
2025-03-08 16:30:45 +0530

    initial

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql2');
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+
+
+
+
+
+
+app.use(express.json());
+app.use(cors());
+
+
+const db = mysql.createConnection({
+    host: process.env.DB_HOST || '195.35.45.44',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || 'vikram123',
+    database: process.env.DB_NAME || 'dreamsguider',
+
+})
+
+
+db.connect(() => {
+    console.log('Connected to the database');
+})
+
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+} 
+);
+
+
+app.post('/addDetails', (req, res) => {
+    const { id, studentName, address, classes, gender, contact } = req.body;
+
+    const sql = 'INSERT INTO batch9(id, stuentName, address, class, gender, contactNo) VALUES (?,?,?,?,?,?)';
+    db.query(sql, [id, studentName, address, classes, gender, contact], (err, result) => {
+        if (err) throw err;
+
+        res.send('Data inserted successfully');
+        console.log('Data inserted successfully');
+    })
+})
