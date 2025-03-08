44485364b08dc40987e6762dda24901681cd530e
dgschooldevelopment <dreamsguider@gmail.com>
2025-03-08 17:29:43 +0530

    cors added

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ dotenv.config();
 
 app.use(express.json());
 app.use(cors({
-    origin: 'http://127.0.0.1:5501', // replace with your frontend url,
+    origin: 'http://127.0.0.1', // replace with your frontend url,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'] // replace with your required headers,
 }));
